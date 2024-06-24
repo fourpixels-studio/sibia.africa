8a748ea6bf7e95a83361b5010e47956f0a876032
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-06-24 21:24:06 +0300

    Second commit 🚀

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import NewsletterSubscription
+from .models import NewsletterSubscription, Homepage
 
 admin.site.register(NewsletterSubscription)
+admin.site.register(Homepage)
--- website/models.py
+++ website/models.py
@@ -7,3 +7,42 @@ class NewsletterSubscription(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class Homepage(models.Model):
+    hero_title = models.CharField(max_length=150, blank=True, null=True)
+    hero_paragraph = models.TextField(blank=True, null=True)
+    hero_primary_btn = models.CharField(max_length=90, blank=True, null=True)
+    hero_secpndary_btn = models.CharField(max_length=90, blank=True, null=True)
+    hero_image = models.ImageField(
+        upload_to="homepage/", blank=True, null=True)
+    quote = models.TextField(blank=True, null=True)
+    feature_1_title = models.CharField(max_length=150, blank=True, null=True)
+    feature_1_paragraph = models.TextField(blank=True, null=True)
+    feature_1_btn = models.CharField(max_length=50, blank=True, null=True)
+    feature_2_title = models.CharField(max_length=150, blank=True, null=True)
+    feature_2_paragraph = models.TextField(blank=True, null=True)
+    feature_2_btn = models.CharField(max_length=50, blank=True, null=True)
+    feature_3_title = models.CharField(max_length=150, blank=True, null=True)
+    feature_3_paragraph = models.TextField(blank=True, null=True)
+    feature_3_btn = models.CharField(max_length=50, blank=True, null=True)
+    feature_4_title = models.CharField(max_length=150, blank=True, null=True)
+    feature_4_paragraph = models.TextField(blank=True, null=True)
+    feature_4_btn = models.CharField(max_length=50, blank=True, null=True)
+    features_image = models.FileField(
+        upload_to="homepage/", blank=True, null=True)
+    newsletter_subscription = models.CharField(
+        max_length=200, blank=True, null=True)
+    about_title = models.CharField(max_length=150, blank=True, null=True)
+    about_paragraph = models.TextField(blank=True, null=True)
+    about_primary_btn = models.CharField(max_length=90, blank=True, null=True)
+    about_image = models.ImageField(
+        upload_to="homepage/", blank=True, null=True)
+    cta = models.CharField(max_length=90, blank=True, null=True)
+
+    def __str__(self):
+        return "Home Page"
+
+    class Meta:
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Page"
