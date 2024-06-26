4ef1724fb664282333908d3f68d36e05f4610862
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-06-26 20:53:38 +0300

    request_appointment first commit

--- website/urls.py
+++ website/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import index, about, contact, newsletter_subscription, help
+from .views import index, about, contact, newsletter_subscription, help, request_appointment
 from .crud_methods import edit_homepage
 
 urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
     path("contact/", contact, name="contact"),
+    path("request-appointment/", request_appointment, name="request_appointment"),
     path("edit/homepage/<int:pk>/", edit_homepage, name="edit_homepage"),
     path("help/", help, name="help"),
-    path("newsletter-subscription/", newsletter_subscription,
-         name="newsletter_subscription"),
+    path("newsletter-subscription/", newsletter_subscription, name="newsletter_subscription"),
 ]
